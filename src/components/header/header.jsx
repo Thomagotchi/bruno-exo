c9e2206ded4976055fa20068cdad6a7bdfad07d4
Thomagotchi <thomassano87@gmail.com>
2024-11-22 11:56:26 +0100

    Animation header + ajout des liens vers les sections de la page

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -11,28 +11,64 @@ export function Header() {
         />
       </div>
 
-      <div className="flex flex-row text-primary-dark-green text-1s gap-24 w-4/12 justify-center opacity-70">
-        <div className="flex gap-8">
-          <p>Solutions</p>
-          <img
-            src="buttons/dropdown-arrow.svg"
+      <nav className="flex flex-row text-primary-dark-green text-1s gap-24 w-4/12 justify-center transition ease-out duration-300">
+        <a
+          href="#home-solutions"
+          className="items-center flex gap-8 opacity-70 hover:cursor-pointer hover:opacity-100 group"
+        >
+          <p className="transition ease-out duration-300">Solutions</p>
+          <svg
+            className="flex group-hover:rotate-180 transition ease-out duration-300"
             alt="Dropdown arrow for solutions menu."
-          />
-        </div>
-        <div className="flex gap-8">
+            width="9"
+            height="6"
+            viewBox="0 0 9 6"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7.98167 1.19447L4.37072 4.80542L0.759766 1.19447"
+              stroke="#3C4A41"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </a>
+        <a
+          href="#home-platform"
+          className="items-center flex gap-8 opacity-70 hover:cursor-pointer hover:opacity-100 group transition ease-out duration-300"
+        >
           <p>Platform</p>
-          <img
-            src="buttons/dropdown-arrow.svg"
-            alt="Dropdown arrow for platform menu."
-          />
-        </div>
-        <div>
+          <svg
+            className="flex group-hover:rotate-180 transition ease-out duration-500"
+            alt="Dropdown arrow for solutions menu."
+            width="9"
+            height="6"
+            viewBox="0 0 9 6"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M7.98167 1.19447L4.37072 4.80542L0.759766 1.19447"
+              stroke="#3C4A41"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            />
+          </svg>
+        </a>
+        <a
+          href="#home-technology"
+          className="opacity-70 hover:cursor-pointer hover:opacity-100"
+        >
           <p>Resources</p>
-        </div>
-        <div>
+        </a>
+        <a
+          href="#home-technology"
+          className="opacity-70 hover:cursor-pointer hover:opacity-100"
+        >
           <p>About</p>
-        </div>
-      </div>
+        </a>
+      </nav>
 
       <div className="ml-auto">
         <DemoButtonPrimary />
